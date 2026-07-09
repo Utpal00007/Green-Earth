1348a5435b8d00d9efbe02d899a479f964807459
Utpal <2022746@iub.edu.bd>
2026-07-09 12:50:45 +0600

    update on js with loadPlants and displayPlants

--- script/script.js
+++ script/script.js
@@ -6,7 +6,15 @@ const loadCategories = () => {
     .then((data) => displayCategories(data.categories));
 };
 
-// display categories
+// load plants
+
+const loadPlants = (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPlants(data.plants));
+};
+// display categories  and  plants
 
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("category-container");
@@ -16,7 +24,7 @@ const displayCategories = (categories) => {
     const categoryBtn = document.createElement("div");
     categoryBtn.innerHTML = ` 
                 <li>
-                  <button
+                  <button onclick= "loadPlants(${category.id})"
                     class="w-full text-[#1F2937] rounded-lg text-center lg:text-left px-3 py-2 rounded-mb"
                   >
                     ${category.category_name}
@@ -27,4 +35,41 @@ const displayCategories = (categories) => {
   });
 };
 
+//display plants
+
+const displayPlants = (plants) => {
+  const plantContainer = document.getElementById("plant-container");
+  plantContainer.innerHTML = "";
+
+  plants.forEach((plant) => {
+    console.log(plant); // Check that each plant is coming
+
+    const plantCard = document.createElement("div");
+
+    plantCard.innerHTML = ` <div class="bg-white rounded-lg p-3">
+              <div class=""><img src="${plant.image}" alt="" class="w-full h-46 rounded-lg object-cover"></div>
+              <div class="space-y-3">
+                <h2 class="mt-3 text-base font-semibold">${plant.name}</h2>
+                <p class="text-sm text-[#1F2937]">
+                  ${plant.description}
+                </p>
+                <div class="flex justify-between mb-3 font-semibold text-sm">
+                  <span class="bg-[#DCFCE7] rounded-xl py-1 px-2"
+                    >${plant.category}</span
+                  >৳
+                  <span>${plant.price}</span>
+                </div>
+              </div>
+              <button
+                class="btn w-full bg-[#15803D] p-3 text-white rounded-full"
+              >
+                Add To Cart
+              </button>
+            </div>`;
+
+    plantContainer.append(plantCard);
+  });
+};
+
 loadCategories();
+loadPlants(3);
